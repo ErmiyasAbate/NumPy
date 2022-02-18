a1d38bf0fff8476cb4f686773e0ad7ba9cd0896b
ErmiyasAbate <ermiyasabateg@gmail.com>
2022-02-18 23:50:40 +0300

    NumPy Array Slicing

---
+++ NumPy Array Slicing/OneD Array Slicing.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(arr[1:7])
+print(arr[:7])
+print(arr[3:])
+print(arr[0:8:2])
+print(arr[1:8:2])
+print(arr[::3])
+
+print(arr[-7:-4])
---
+++ NumPy Array Slicing/TwoD Array Slicing.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(arr)
+print(arr[0, 2])
+print(arr[0, 2:4])
+print(arr[1, 3:])
+
+print(arr[0:2, 2])
+print(arr[0:2, 2:4])
